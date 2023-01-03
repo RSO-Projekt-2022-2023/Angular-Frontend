2f731a8dc0367c55e2aea9df162cfa821c480288
Enej <enej.bacic@gmail.com>
2023-01-03 15:05:58 +0100

    Popravljen nek napačen vnos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
+
 import { LocalService } from './local/local.service';
-=======
+
 import {NotificationsService} from "./services/notiifications/notifications.service";
->>>>>>> 9ba0da8a179f8a6b97c6ce2615981463b9b40427
 
 @Component({
   selector: 'app-root',
@@ -13,10 +12,9 @@ import {NotificationsService} from "./services/notiifications/notifications.serv
 export class AppComponent {
   title = 'Polnilnice';
 
-<<<<<<< HEAD
+
   constructor(private localStore: LocalService) {
 
   }
-=======
->>>>>>> 9ba0da8a179f8a6b97c6ce2615981463b9b40427
+
 }
